84c3a4a94ec7995a5e68557f1ef8cea02527ee74
sxj754 <sxj754@case.edu>
2023-03-09 16:34:07 -0500

    Main page function

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,20 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navigation from "components/Navigation";
 import "css/main.css";
 
 const Main = () => {
   return (
-    <div className="container-fluid p1-relative">
-      <div className="container">
-        <Navigation />
-        <div className="p1-wrapper">
-          <div className="p1-hero">
-            <p className="p1-hero__txt">
-              창업에 열망이 있는 모든 연세대학생들에게
-              <br />
-              <span>미래</span>를 열어주는 벤처창업동아리
-            </p>
-          </div>
+    <div className="container-fluid">
+      <Navigation />
+      <div className="m-social">
+        <p>youtube</p>
+        <p>blog</p>
+      </div>
+      <div className="m-flex">
+        <div className="m-links">
+          <Link to="/">
+            <p>Main</p>
+          </Link>
+          <Link to="/books">
+            <p>Books</p>
+          </Link>
+          <Link to="/about">
+            <p>About Us</p>
+          </Link>
+          <Link to="/contact">
+            <p>Contact</p>
+          </Link>
+        </div>
+        <div>
+          <p className="m-copyright">
+            COPYRIGHT XINGZHI BOOKS AL RIGHT RESERVED
+          </p>
         </div>
       </div>
     </div>
