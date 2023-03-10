3e9ca3ae1a296d37744249962d2ed9cf1e1d0dbc
sxj754 <sxj754@case.edu>
2023-03-09 19:56:29 -0500

    Create book page

--- src/routes/Books.js
+++ src/routes/Books.js
@@ -6,8 +6,18 @@ const Books = () => {
   return (
     <div className="container-fluid">
       <div className="b-flex">
-        <img src={bookimg} alt="book" className="b-img" />
-        <p className="b--title">Apply</p>
+        <div className="b-left">
+          <p className="b-title">Books</p>
+          <p className="b-subtitle">고묘고종록</p>
+          <img src={bookimg} alt="bookimg" />
+        </div>
+        <div className="b-right">
+          <p className="b-sectiontitle">저자와 독자</p>
+          <p className="b-sectionbody">책을 만드는 일이 어렵다고 합니다.</p>
+          <p className="b-sectionfooter">
+            도서출판 선행후지 대표 <span>임형택</span>
+          </p>
+        </div>
       </div>
     </div>
   );
