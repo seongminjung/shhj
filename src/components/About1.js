756b5e29ad46ff0dd2c7a3d95ba7e7c875cf0cfa
sxj754 <sxj754@case.edu>
2023-03-09 19:44:41 -0500

    About page 1

--- src/components/About1.js
+++ src/components/About1.js
@@ -4,8 +4,20 @@ import "css/about1.css";
 const About1 = () => {
   return (
     <>
-      <div className="container">
-        <p className="r1-title">- Recruit -</p>
+      <div className="container-fluid">
+        <div className="a1-flex">
+          <div className="a1-left">
+            <p className="a1-title">About Us</p>
+            <p className="a1-subtitle">인사말</p>
+          </div>
+          <div className="a1-right">
+            <p className="a1-sectiontitle">저자와 독자</p>
+            <p className="a1-sectionbody">책을 만드는 일이 어렵다고 합니다.</p>
+            <p className="a1-sectionfooter">
+              도서출판 선행후지 대표 <span>임형택</span>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
