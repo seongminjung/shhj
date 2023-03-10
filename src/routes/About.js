9064fed5845aea3be1380ef8e6b72e7456cade00
sxj754 <sxj754@case.edu>
2023-03-10 18:55:02 -0500

    Add text and carousel

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,17 +1,53 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import About1 from "components/About1";
 import About2 from "components/About2";
 import "css/about.css";
 
 const About = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
-    <div>
-      <div className="about1">
+    <div className="a-carousel">
+      <Carousel
+        responsive={responsive}
+        swipeable={false}
+        draggable={false}
+        infinite={true}
+        // arrows={false}
+        showDots={true}
+        autoPlay={false}
+        // autoPlaySpeed={3000}
+        renderButtonGroupOutside={true}
+        // customButtonGroup={<ButtonGroup />}
+        // customLeftArrow={<CustomLeftArrow />}
+        // customRightArrow={<CustomRightArrow />}
+        // keyBoardControl={false}
+        // ssr={true} // means to render carousel on server-side.
+        // customTransition="all .5"
+        // transitionDuration={500}
+        // containerClass="carousel-container"
+        // removeArrowOnDeviceType={["tablet", "mobile"]}
+        // deviceType={this.props.deviceType}
+        // dotListClass="custom-dot-list-style"
+        // itemClass="carousel-item-padding-40-px"
+      >
         <About1 />
-      </div>
-      <div className="about2">
         <About2 />
-      </div>
+      </Carousel>
     </div>
   );
 };
