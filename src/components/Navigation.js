63c8fe5ac14b67e91b977c65c046922c3cdac23d
sxj754 <sxj754@case.edu>
2023-03-23 13:41:28 -0400

    Add kor_chi converter

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "css/navigation.css";
 import logo from "img/logo/logo_black.png";
 import bookmark_dot from "img/asset/bookmark_dot.png";
+import kor_ver from "img/asset/kor_ver.png";
 
 const Navigation = () => {
   return (
@@ -10,11 +11,10 @@ const Navigation = () => {
       <Link to="/">
         <img src={logo} alt="logo" className="n-logo" />
       </Link>
-      <div className="n-language">
-        <p>kor</p>
-        <p>chn</p>
+      <div>
+        <img src={kor_ver} alt="kor_ver" className="n-language" />
       </div>
-      <img src={bookmark_dot} alt={"bookmark_dot"} className="n-bookmark" />
+      <img src={bookmark_dot} alt="bookmark_dot" className="n-bookmark" />
     </div>
   );
 };
