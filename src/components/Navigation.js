3c92ae3484304a9e325761f10fac6b228cda85bf
sxj754 <sxj754@case.edu>
2023-04-01 14:07:28 -0400

    Toggle nav image between Korean and Chinese

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "css/navigation.css";
 import logo from "img/logo/logo_black.png";
 import bookmark_dot from "img/asset/bookmark_dot.png";
 import kor_ver from "img/asset/kor_ver.png";
+import chn_ver from "img/asset/chn_ver.png";
 
 const Navigation = () => {
+  const [lan, setLen] = useState(kor_ver);
+  const toggleLanguage = () => {
+    if (lan === kor_ver) {
+      setLen(chn_ver);
+    } else {
+      setLen(kor_ver);
+    }
+  };
   return (
     <div className="n-wrapper">
       <Link to="/">
         <img src={logo} alt="logo" className="n-logo" />
       </Link>
       <div>
-        <img src={kor_ver} alt="kor_ver" className="n-language" />
+        <img
+          src={lan}
+          alt="language"
+          className="n-language"
+          onClick={toggleLanguage}
+        />
       </div>
       <img src={bookmark_dot} alt="bookmark_dot" className="n-bookmark" />
     </div>
