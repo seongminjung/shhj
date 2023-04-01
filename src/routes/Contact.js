b8ddb2a24c3b2633cdc74ad1da3d6b5b779cdaf1
sxj754 <sxj754@case.edu>
2023-04-01 15:52:06 -0400

    Change contact design

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -11,15 +11,13 @@ const contact = () => {
         alt="background"
         className="backgroundimg_full"
       />
+      <p className="c-title">Contact</p>
       <div className="c-flex">
         <div className="c-left">
-          <p className="c-title">Contact</p>
-          <div className="c-details">
-            <p>주소 | [10881] 경기도 파주시 회동길 210</p>
-            <p>전화 | 031 955 1936, 2560</p>
-            <p>팩스 | 031 955 2557</p>
-            <p>독자의견 | bookpot@hanmail.net</p>
-          </div>
+          <p>주소 | [10881] 경기도 파주시 회동길 210</p>
+          <p>전화 | 031 955 1936, 2560</p>
+          <p>팩스 | 031 955 2557</p>
+          <p>독자의견 | bookpot@hanmail.net</p>
         </div>
         <div className="c-right">
           <img src={contactimg} alt="contact" />
