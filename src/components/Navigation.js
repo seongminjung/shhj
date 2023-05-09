111d7e9eba113222c8e271566f9d002b640ff61d
sxj754 <sxj754@case.edu>
2023-05-09 16:50:54 -0400

    Remove language switch

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "css/navigation.css";
 import logo from "img/logo/logo_black.png";
 import bookmark_dot from "img/asset/bookmark_dot.png";
-import kor_ver from "img/asset/kor_ver.png";
-import chn_ver from "img/asset/chn_ver.png";
+// import kor_ver from "img/asset/kor_ver.png";
+// import chn_ver from "img/asset/chn_ver.png";
 
 const Navigation = () => {
-  const [lan, setLen] = useState(kor_ver);
-  const toggleLanguage = () => {
-    if (lan === kor_ver) {
-      setLen(chn_ver);
-    } else {
-      setLen(kor_ver);
-    }
-  };
+  // const [lan, setLen] = useState(kor_ver);
+  // const toggleLanguage = () => {
+  //   if (lan === kor_ver) {
+  //     setLen(chn_ver);
+  //   } else {
+  //     setLen(kor_ver);
+  //   }
+  // };
   return (
     <div className="n-wrapper">
       <Link to="/main">
         <img src={logo} alt="logo" className="n-logo" />
       </Link>
-      <div>
+      {/* <div>
         <img
           src={lan}
           alt="language"
           className="n-language"
           onClick={toggleLanguage}
         />
-      </div>
+      </div> */}
       <Link to="/">
         <img src={bookmark_dot} alt="bookmark_dot" className="n-bookmark" />
       </Link>
